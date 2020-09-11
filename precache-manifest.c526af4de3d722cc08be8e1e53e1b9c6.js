241b12ea6ce54b3945abc17d5db133ae13829290
Hong Quan <quandh619@gmail.com>
2020-09-11 15:20:39 +0700

    Updates

---
+++ precache-manifest.c526af4de3d722cc08be8e1e53e1b9c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cbd5279f6746a4153ff2f7b0aff391",
+    "url": "/Hospital_Dapp/index.html"
+  },
+  {
+    "revision": "3216c39aeb940f4635b7",
+    "url": "/Hospital_Dapp/static/css/main.64e66807.chunk.css"
+  },
+  {
+    "revision": "20ff40be5059d0c6913a",
+    "url": "/Hospital_Dapp/static/js/2.afdec703.chunk.js"
+  },
+  {
+    "revision": "49423731f4b42bb1acb1fefa8741c67d",
+    "url": "/Hospital_Dapp/static/js/2.afdec703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3216c39aeb940f4635b7",
+    "url": "/Hospital_Dapp/static/js/main.fb715244.chunk.js"
+  },
+  {
+    "revision": "e5af661012e7b204a1f0",
+    "url": "/Hospital_Dapp/static/js/runtime-main.cef3d7bd.js"
+  }
+]);
